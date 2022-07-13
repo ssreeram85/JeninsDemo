4c134227dc68e6ebc146561f3c87df98e3c4f411
Sreeram Sanepalli <sanepalli.sreeram@gmail.com>
2022-07-13 11:47:36 -0700

    first commit

---
+++ src/main/java/com/sree/tech/JeninsDemoApplication.java
@@ -0,0 +1,22 @@
+package com.sree.tech;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeninsDemoApplication {
+	
+	private static Logger logger = LoggerFactory.getLogger(JeninsDemoApplication.class);
+	
+	public void intt() {
+		logger.info("Application started with init ...");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application started with main ...");
+		SpringApplication.run(JeninsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sree/tech/JeninsDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sree.tech;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeninsDemoApplicationTests {
+	private static Logger logger = LoggerFactory.getLogger(JeninsDemoApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case executed ...");
+	}
+
+}
